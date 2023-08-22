78cac445f5578711a353b967d956e2457b409379
Christian Enderle <mail@chrenderle.de>
2023-08-22 21:59:34 +0200

    setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,4 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chrenderle.de',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
 });
